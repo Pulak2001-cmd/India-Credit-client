65e9fcbe1c320eca5177c15809da35cf4d7ce544
Aryapriyo Mandal <75712204+alphawolf07@users.noreply.github.com>
2022-04-09 01:13:45 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import React,{Fragment} from 'react'
 
-function Card() {
+function Card(props) {
   return (
     <Fragment>
-          <div class="card" style={{width: "18rem"}}>
-    <img src="./Image1.png" class="card-img-top" alt="..."/>
+         <div class="card" style={{width: "18rem"}}>
+    <img src={props.src} class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title">{props.title}</h5>
+      <p class="card-text">{props.text}</p>
+      <a href="#" class="btn btn-primary">{props.btn}</a>
     </div>
   </div>
     </Fragment>
