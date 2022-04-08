a46d1162718df87cb38cd86236670a8abe9ae722
Aryapriyo Mandal <mandalaryapriyo@gmail.com>
2022-04-09 01:26:19 +0530

    commit fifth

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ import Card from './Card'
 function Cards() {
   return (
     <Fragment>
-        <div class="container my-2">
+        <div class="container my-3">
   <div class="row">
     <div class="col">
       <Card src="./Image1.png" title="Card Title" text="Some quick example text to build on the card title and make up the bulk of the card's content." btn="Link"/>
