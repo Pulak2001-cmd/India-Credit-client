37641aa738612cd5a039b123001666e32918ac18
Aryapriyo Mandal <mandalaryapriyo@gmail.com>
2022-04-09 15:57:04 +0530

    commit seventh

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import Header from './components/Header'
 import Cards from './components/Cards';
 import Intro from './components/Intro';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />
       <Intro />
-      <Cards/>
+      <Cards/> */}
+      <Register />
     </div>
   );
 }
